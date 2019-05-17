c65cec4ef26a6dbb277665945e0ca23279b43fdd
francis_chukwuma <francischukwuma@PSADMINs-MacBook-Air.local>
2019-05-17 13:04:54 -0400

    added all components and form

--- project3-app/src/App.js
+++ project3-app/src/App.js
@@ -11,7 +11,23 @@ class App extends Component {
     return (
       <Router>
         <div className="container">
-          <h2>Francis Teaching Hospital</h2>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="" target="_blank">
+        <img src={logo} width="30" height="30" alt="Logo missing"/>
+        <Link to="/" className="navbar-brand">Francis Teaching Hospital</Link>
+        <div className="collapse nav-collapse">
+        <ul className="navbar-nav mr-auto">
+        <li className="navbar-item">
+        <Link to="/" className="navbar-link">Doctors</Link>
+        </li>
+        <li className="navbar-item">
+        <Link to="/create" className="navbar-link">Create DoctorsList</Link>
+        </li>
+
+        </ul>
+        </div>
+        </a>
+        </nav>
           <Route path="/" exact component={DoctorsList} />
           <Route path="/edit/:id" exact component={EditDoctors} />
           <Route path="/create" exact component={CreateDoctors} />
--- project3-app/src/components/create-Doctors.js
+++ project3-app/src/components/create-Doctors.js
@@ -1,12 +1,110 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 export default class CreateDoctorsList extends Component {
-    render(){
-        return(
-            <div>
-                <p>Welcome To Francis Teaching Hospital</p>
-                <p>Create Doctors</p>
-            </div>
+
+    constructor(props) {
+        super(props);
+
+        this.onChangeDoctorsName = this.onChangeDoctorsName.bind(this);
+        this.onChangeDoctorsPicture = this.onChangeDoctorsPicture.bind(this);
+        this.onChangeDoctorsDescription = this.onChangeDoctorsDescription.bind(this);
+        this.onChangeDoctorsInformation = this.onChangeDoctorsInformation.bind(this);
+        this.onChangeDoctorsAvailability = this.onChangeDoctorsAvailability.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        this.state = {
+            Doctors_name: "",
+            Doctors_picture: "",
+            Doctors_description: "",
+            Doctors_information: "",
+            Doctors_availability: "",
+        }
+    }
+    onChangeDoctorsName(e) {
+        this.setState({
+            Doctors_name: e.target.value
+        });
+    }
+
+    onChangeDoctorsPicture(e) {
+        this.setState({
+            Doctors_picture: e.target.value
+        });
+    }
+
+    onChangeDoctorsDescription(e) {
+        this.setState({
+            Doctors_description: e.target.value
+        });
+    }
+
+    onChangeDoctorsInformation(e) {
+        this.setState({
+            Doctors_information: e.target.value
+        });
+    }
+
+    onChangeDoctorsAvailability(e) {
+        this.setState({
+            Doctors_availability: e.target.value
+        });
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+
+        console.log(`Form submitted:`);
+        console.log(`Doctors Description: ${this.state.Doctors_description}`);
+        console.log(`Doctors Name: ${this.state.Doctors_name}`);
+        console.log(`Doctors Information: ${this.state.Doctors_information}`);
+        console.log(`Doctors Picture: ${this.state.Doctors_picture}`);
+        console.log(`Doctors Availability: ${this.state.Doctors_availability}`)
+        this.state = ({
+            Doctors_name: "",
+            Doctors_picture: "",
+            Doctors_description: "",
+            Doctors_information: "",
+            Doctors_availability: "",
+        });
+    }
+    render() {
+        return (
+            <div style={{ marginTop: 25 }}>
+                <h3>Create New Doctor</h3>
+                <form onSubmit={this.onSubmit}>
+                    <div className="form-group">
+                        <label>Name</label>
+                        <input type="text"
+                            className="form-control"
+                            value={this.state.Doctors_name}
+                            onChange={this.onChangeDoctorsName} />
+
+                        <label>Picture</label>
+                        <input type="text"
+                            className="form-control"
+                            value={this.state.Doctors_picture}
+                            onChange={this.onChangeDoctorsPicture} />
+
+                        <label>Description</label>
+                        <input type="text"
+                            className="form-control"
+                            value={this.state.Doctors_description}
+                            onChange={this.onChangeDoctorsDescription} />
+
+                        <label>Information</label>
+                        <input type="text"
+                            className="form-control"
+                            value={this.state.Doctors_information}
+                            onChange={this.onChangeDoctorsInformation} />
+
+                        <label>Availability</label>
+                        <input type="text"
+                            className="form-control"
+                            value={this.state.Doctors_availability}
+                            onChange={this.onChangeDoctorsAvailability} />
+                    </div>
+                </form>
+            </div >
         )
     }
 }
